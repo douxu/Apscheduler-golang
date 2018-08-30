e3a8c31b4810f51bd1e7058732ab57f124464607
窦旭 <douxu@douxudeMacBook-Pro.local>
2018-08-30 08:09:09 +0800

    init message struct and heap struct

---
+++ models/head.go
@@ -0,0 +1,7 @@
+package models
+
+// Message respresent the message struct
+type Message struct {
+	CreateTime int64
+	Status     int
+}
---
+++ utils/sort.go
@@ -0,0 +1,6 @@
+package utils
+
+import "Apscheduler-golang/models"
+
+// MessageHeap respresent the base struct in heap
+type MessageHeap []models.Message
